6dc10533806ea91ffd27da32d1ca5569de0f179b
Peter Duff <pdu@ihtsdo.org>
2020-02-03 11:35:49 +0000

    MRCMMT-160 User name only

--- src/app/components/snomed-navbar/snomed-navbar.component.ts
+++ src/app/components/snomed-navbar/snomed-navbar.component.ts
@@ -3,6 +3,8 @@ import { Subscription } from 'rxjs';
 import { SnomedResponseObject } from '../../models/snomedResponseObject';
 import { BranchingService } from '../../services/branching.service';
 import { MrcmmtService } from '../../services/mrcmmt.service';
+import { User } from '../../models/user';
+import { AuthenticationService } from '../../services/authentication.service';
 
 @Component({
     selector: 'app-snomed-navbar',
@@ -16,14 +18,19 @@ export class SnomedNavbarComponent implements OnInit {
     private branchPathSubscription: Subscription;
     private versions: SnomedResponseObject;
     private versionsSubscription: Subscription;
+    private user: User;
+    private userSubscription: Subscription;
 
-    constructor(private branchingService: BranchingService, private mrcmmtService: MrcmmtService) {
+    constructor(private branchingService: BranchingService, private mrcmmtService: MrcmmtService,
+                private authenticationService: AuthenticationService) {
         this.environment = window.location.host.split(/[.]/)[0].split(/[-]/)[0];
         this.branchPathSubscription = this.branchingService.getBranchPath().subscribe(data => {
             this.branchPath = data;
             this.mrcmmtService.setupDomains();
         });
         this.versionsSubscription = this.branchingService.getVersions().subscribe(data => this.versions = data);
+        this.versionsSubscription = this.branchingService.getVersions().subscribe(data => this.versions = data);
+        this.userSubscription = this.authenticationService.getLoggedInUser().subscribe(data => this.user = data);
     }
 
     ngOnInit() {
