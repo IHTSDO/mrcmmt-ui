1e36d02fed9cb69a7564d207dd602177e829e672
Peter Duff <pdu@ihtsdo.org>
2020-03-18 09:15:13 +0000

    MRCMMT-204 Found and removed source of duplicate panel loads

--- src/app/services/mrcmmt.service.ts
+++ src/app/services/mrcmmt.service.ts
@@ -116,6 +116,5 @@ export class MrcmmtService {
         this.domainService.clearDomainFilter();
         this.attributeService.clearAttributeFilter();
         this.urlParamsService.updateActiveRefsetParams(null, null, null);
-        this.setupDomains();
     }
 }
