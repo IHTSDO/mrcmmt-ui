ec3bb4df98f21bbccd7382a7d1a153bdcf4e2d92
Chris Swires <csw@ihtsdo.org>
2020-11-05 15:08:02 +0000

    CDI-75 - shifted CDI visibility behind login

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -109,10 +109,10 @@ export class AppComponent implements OnInit {
 
                     if (user.roles.includes('ROLE_int-sca-author')) {
                         versions.items.push({branchPath: 'MAIN/MRCMMAINT1'});
+                        versions.items.push({branchPath: 'MAIN/CDITEST1'});
                     }
 
                     versions.items.push({branchPath: 'MAIN'});
-                    versions.items.push({branchPath: 'MAIN/CDITEST1'});
 
                     versions.items.reverse();
 
